83bc06cdfed5549df20342ab1ffb82b46c0811f8
Silvio Ruiz <silviojrck@gmail.com>
2023-07-11 12:54:44 -0300

    agrega middlewares en index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
+
 
 const app = express();
 
@@ -7,3 +11,7 @@ app.listen(app.get('port'), () => {
     console.log('app running on port', app.get('port'));
 });
 
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
